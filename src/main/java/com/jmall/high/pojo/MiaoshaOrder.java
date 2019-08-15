9dfee1081ca1cf72ca47b94126c80b06d6c6b2a6
uzig305 <zjt_james@163.com>
2019-08-16 00:09:49 +0800

    pojo

---
+++ src/main/java/com/jmall/high/pojo/MiaoshaOrder.java
@@ -0,0 +1,36 @@
+/**
+ * created by Zheng Jiateng on 2019/8/6.
+ */
+
+package com.jmall.high.pojo;
+
+public class MiaoshaOrder {
+	private Long id;
+	private Long userId;
+	private Long  orderId;
+	private Long goodsId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public Long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+	public Long getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(Long goodsId) {
+		this.goodsId = goodsId;
+	}
+}
