48be064be7501ff7ab9aec314ea4356a70fd9889
uzig305 <zjt_james@163.com>
2019-08-23 22:54:31 +0800

    页面静态化

--- src/main/java/com/jmall/high/controller/GoodsController.java
+++ src/main/java/com/jmall/high/controller/GoodsController.java
@@ -7,8 +7,10 @@ package com.jmall.high.controller;
 import com.jmall.high.pojo.MiaoshaUser;
 import com.jmall.high.redis.GoodsKey;
 import com.jmall.high.redis.RedisService;
+import com.jmall.high.result.Result;
 import com.jmall.high.service.GoodsService;
 import com.jmall.high.service.UserService;
+import com.jmall.high.vo.GoodsDetailVo;
 import com.jmall.high.vo.GoodsVo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -109,5 +111,36 @@ public class GoodsController {
             redisService.set(GoodsKey.getGoodsDetail, ""+goodsId, html);
         }
         return html;
+    }
+
+    /**
+     * 页面静态化
+     */
+    @RequestMapping(value="/detail/{goodsId}")
+    @ResponseBody
+    public Result<GoodsDetailVo> detail(HttpServletRequest request, HttpServletResponse response, Model model, MiaoshaUser user,
+                                        @PathVariable("goodsId")long goodsId) {
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+        int miaoshaStatus = 0;
+        int remainSeconds = 0;
+        if(now < startAt ) {//秒杀还没开始，倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int)((startAt - now )/1000);
+        }else  if(now > endAt){//秒杀已经结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        }else {//秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+        GoodsDetailVo vo = new GoodsDetailVo();
+        vo.setGoods(goods);
+        vo.setUser(user);
+        vo.setRemainSeconds(remainSeconds);
+        vo.setMiaoshaStatus(miaoshaStatus);
+        return Result.success(vo);
     }
 }
