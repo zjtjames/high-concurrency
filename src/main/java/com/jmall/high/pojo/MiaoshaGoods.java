f96e2c5ce7436040c20d41ece1cf6fdeef2f6600
uzig305 <zjt_james@163.com>
2019-08-27 15:44:27 +0800

    getter & setter

--- src/main/java/com/jmall/high/pojo/MiaoshaGoods.java
+++ src/main/java/com/jmall/high/pojo/MiaoshaGoods.java
@@ -12,33 +12,43 @@ public class MiaoshaGoods {
 	private Integer stockCount;
 	private Date startDate;
 	private Date endDate;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Long getGoodsId() {
 		return goodsId;
 	}
+
 	public void setGoodsId(Long goodsId) {
 		this.goodsId = goodsId;
 	}
+
 	public Integer getStockCount() {
 		return stockCount;
 	}
+
 	public void setStockCount(Integer stockCount) {
 		this.stockCount = stockCount;
 	}
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public Date getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
